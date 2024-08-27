6dcaf623e58502a841c6c073d6a145942a2f49b9
Sly-Harvey <86175780+Sly-Harvey@users.noreply.github.com>
2024-08-27 03:25:27 +0100

    added timer resolution

--- Restore.bat
+++ Restore.bat
@@ -25,12 +25,14 @@ if '%errorlevel%' NEQ '0' (
 
 reg delete "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" /f 2>nul
 
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v GlobalTimerResolutionRequests /t REG_DWORD /d 0x0 /f
+
 reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarSi /f 2>nul
 
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" /v NetworkThrottlingIndex /t REG_DWORD /d 0xA /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" /v SystemResponsiveness /t REG_DWORD /d 0x14 /f
 
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\kbdclass\Parameters" /v KeyboardDataQueueSize /t REG_DWORD /d 64 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\kbdclass\Parameters" /v KeyboardDataQueueSize /t REG_DWORD /d 0x64 /f
 reg delete "HKLM\SYSTEM\CurrentControlSet\Services\mouclass\Parameters" /f 2>nul
 
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile\Tasks\Games" /v "GPU Priority" /t REG_DWORD /d 0x8 /f
@@ -42,14 +44,28 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Control\PriorityControl" /v Win32Priority
 
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\PrefetchParameters" /v EnablePrefetcher /t REG_DWORD /d 0x3 /f
 
-reg add "HKCU\Control Panel\Mouse" /v MouseHoverTime /t REG_SZ /d 400 /f
+reg add "HKCU\Control Panel\Mouse" /v MouseHoverTime /t REG_SZ /d "400" /f
 
-reg add "HKCU\Control Panel\Desktop" /v MenuShowDelay /t REG_SZ /d 1 /f
+reg add "HKCU\Control Panel\Desktop" /v MenuShowDelay /t REG_SZ /d "1" /f
 
-echo All registry modifications have been applied.
 echo.
-echo For some changes to take effect explorer will need to be restarted
-echo Press any key to restart explorer . . .
-pause >nul
-taskkill /im explorer.exe /f
+
+tasklist /fi "ImageName eq SetTimerResolution.exe" /fo csv 2>NUL | find /I "SetTimerResolution.exe">NUL
+if "%ERRORLEVEL%"=="1" (
+    echo SetTimerResolution.exe is already disabled and removed!
+    :: echo if you wish to install and enable it again, use Tweaks.bat
+    echo.
+)
+taskkill /f /im SetTimerResolution.exe 1>nul 2>nul
+del /Q "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\SetTimerResolution.exe.lnk" 1>nul 2>nul
+del /Q "C:\Windows\SetTimerResolution.exe" 1>nul 2>nul
+
+echo Restarting Explorer...
+taskkill /im explorer.exe /f 1>nul 2>nul
 start explorer.exe
+echo Successfully restored all tweaks!
+echo.
+echo REBOOT REQUIRED
+echo Press any key to close...
+pause >nul
+exit
